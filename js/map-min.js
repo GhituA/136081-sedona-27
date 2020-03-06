3b8e3c527a618348717ab5d51e11c14cc572e3e4
GhituA <57188128+GhituA@users.noreply.github.com>
2020-03-06 14:27:48 +0200

    Вносит правки в интерактивные элементы

--- js/map-min.js
+++ js/map-min.js
@@ -1,1 +1,1 @@
-function initMap(){var e={lat:34.869,lng:-111.76},n=new google.maps.Map(document.getElementById("map"),{zoom:9,center:e});new google.maps.Marker({position:e,map:n})}
+function initMap(){var e={lat:34.869,lng:-111.76},o=new google.maps.Map(document.querySelector(".index-map"),{zoom:9,center:e});new google.maps.Marker({position:e,map:o})}
--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 function initMap() {
-var sedona = {lat: 34.869, lng: -111.760};
-var map = new google.maps.Map(
-  document.getElementById("map"), {zoom: 9, center: sedona});
-var marker = new google.maps.Marker({position: sedona, map: map});
+  var sedona = {lat: 34.869, lng: -111.760};
+  var map = new google.maps.Map(document.querySelector(".index-map"), {zoom: 9, center: sedona});
+  var marker = new google.maps.Marker({position: sedona, map: map});
 }
